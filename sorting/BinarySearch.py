0ce45297dce2a1be2daa9e9b1477b78bbbb9751b
Anna <ania.piktas@gmail.com>
2019-02-19 09:41:02 +0000

    Removing temp changes to binary sorting

--- sorting/BinarySearch.py
+++ sorting/BinarySearch.py
@@ -1,28 +1,23 @@
 import math
 
-#
-# def bin_search(li, element):
-#     bottom = 1
-#     top = len(li)
-#     index = 0
-#     while top >= bottom: #option without break: and index == 0
-#         mid = int(math.floor((top+bottom)/2.0))
-#         if li[mid] == element:
-#             index = mid
-#             break
-#         elif li[mid] > element:
-#             top = mid-1
-#         else:
-#             bottom = mid+1
-#
-#     return index
-#
-# li = [2,5,7,9,11,17,222]
-# print(bin_search(li, 11))
-# print(bin_search(li, 2))
 
-a = repr(math.pow(2, 63) - 1)
-print(a)
+def bin_search(li, element):
+    bottom = 1
+    top = len(li)
+    index = 0
+    while top >= bottom: #option without break: and index == 0
+        mid = int(math.floor((top+bottom)/2.0))
+        if li[mid] == element:
+            index = mid
+            break
+        elif li[mid] > element:
+            top = mid-1
+        else:
+            bottom = mid+1
 
-9223372036854775808
-9223372036854775807
+    return index
+
+li = [2,5,7,9,11,17,222]
+print(bin_search(li, 11))
+print(bin_search(li, 2))
+
